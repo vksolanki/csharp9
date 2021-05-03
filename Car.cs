4203b55eabfdf307db95d30bcab242d7b51383d3
Vikas Solanki <48885907+vksolanki@users.noreply.github.com>
2021-05-03 23:18:58 +0530

    Create Car.cs

---
+++ Car.cs
@@ -0,0 +1,26 @@
+namespace CsharpDemo
+{
+    public class Car
+    {
+        public string Name { get; set; }
+    }
+
+    public class CarFactory
+    {
+        public static Car GetCar()
+        {
+            Car car1 = new Car() { Name = "Your Favourit Car" };
+            return car1;
+        }
+        public static IEnumerable<Car> GetCars()
+        {
+            IList<Car> cars = new List<Car>()
+            {
+                new Car() { Name = "My Car 1" },
+                new Car() { Name = "My Car 2" },
+            };
+            return cars;
+        }
+
+    }
+}
