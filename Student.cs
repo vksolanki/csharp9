1becb72f5631608308a86f087f84ba8ceb382994
Vikas Solanki <48885907+vksolanki@users.noreply.github.com>
2021-05-07 22:44:58 +0530

    Create Student.cs

---
+++ Student.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWeb
+{
+
+    public class Department
+    {
+        public string Name { get; set; }
+    }
+
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+    }
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public Department Department { get; set; }
+        public Address Address { get; set; }
+    }
+
+    public class StudentFactory
+    {
+        public Student GetStudent()
+        {
+            Student student = new Student()
+            {
+                Name = "Smith",
+                Address = new Address()
+                {
+                    City = "NY",
+                    Street = "Street"
+                },
+                Department = new Department()
+                {
+                    Name = "IT"
+                }
+            };
+
+            return student;
+        }
+    }
+}
