5d14a4e192ca69bee563532f6ea7cf3dfc2ec141
Vikas Solanki <48885907+vksolanki@users.noreply.github.com>
2021-05-21 13:17:26 +0530

    Update Pattern.cs

--- Pattern.cs
+++ Pattern.cs
@@ -1,4 +1,13 @@
- static decimal CashBack(int total) =>
+        static decimal CashBack(int total)
+        {
+            if (total >= 1000) return 100;
+            else if (total > 800 && total < 1000) return 50;
+            else if (total > 200 && total <= 800) return 30;
+            else if (total == 200) return 10;
+            else { return 0; }
+        }
+       // Above code using C# pattern matching (relational patterns)    
+       static decimal CashBack(int total) =>
             total switch
             {
                 >= 1000 => 100,
@@ -7,12 +16,3 @@
                 200 => 10,
                 _ => 0
             };
-
-        //static decimal CashBack(int total)
-        //{
-        //    if (total >= 1000) return 100;
-        //    else if (total > 800 && total < 1000) return 50;
-        //    else if (total > 200 && total <= 800) return 30;
-        //    else if (total == 200) return 10;
-        //    else { return 0; }
-        //}
